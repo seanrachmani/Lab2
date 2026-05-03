199fe214cad06e3fb9383c08a73d741df8538fc0
Sean-VM <sicsicrr11@gmail.com>
2026-05-03 16:21:18 +0300

    0a theoretical

--- myshell.c
+++ myshell.c
@@ -8,7 +8,8 @@
 
 
 
-/*task 0a:
+/*
+==========task 0a:==========
 ==========execute==========
 PRE:receives a parsed line
 POST:invokes the program specified in the cmdLine using the proper system call
@@ -16,15 +17,32 @@ EXECV:int execv(const char* path,char* const argv[])
 path is saved in argv[0](parse did it for us), returns only in case of an error
 */
 void execute(cmdLine* pCmdLine){
-    int error = execv(pCmdLine->arguments[0],pCmdLine->arguments);
+    int error = execvp(pCmdLine->arguments[0],pCmdLine->arguments);
     if(error == -1){
         perror("error:");
         freeCmdLines(pCmdLine);
         exit(1);
     }
 }
-
-
+/*
+==========0a theoretical==========
+1)deleting main memory:
+Q:Although you loop infinitely, the execution ends after execv. Why is that?
+A:successful execv deletes current process memory includes our main & infinity loop,
+and the current PID is changing to the code of the new command we parsed. 
+2)full path:
+Q:You must place the full path of an executable file in-order to run properly. why?
+A:bc execv search the command in the directory we are currently in.
+and there is no basic commands in the lab folder. 
+3)execvp:
+-after replacing to execvp no need for full path sunce execvp knows variable named PATH
+that saving default folders for basic commands. so just ls working
+-Q:Wildcards, as in "ls *", are not working. (Again, why?)
+A:bc wildcards being maintained by shell and not by os,
+and right now our shell doesnt have this feature. 
+for example bash sending collecting file/folders names in current folder and 
+sending the os ls command with this list so we get all subfolders too.
+*/
 
 /*
 ===========myNotes==========
