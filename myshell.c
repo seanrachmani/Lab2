d8cab274a2da3178d27a66b47217feaea78cef69
Sean-VM <sicsicrr11@gmail.com>
2026-05-03 14:58:03 +0300

    0a

--- myshell.c
+++ myshell.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <linux/limits.h>
 #include "LineParser.h"
 
-while(1){
-    fprintf(stdout,)
+
+//task 0a:
+//receives a parsed line
+//invokes the program specified in the cmdLine using the proper system call (see man execv).
+execute(cmdLine *pCmdLine)
+
+
+
+
+int main(int argc, char **argv){
+    while(1){
+        char buf[PATH_MAX];
+        char* path = getcwd(buf,PATH_MAX);
+        fprintf(stdout,"current working directory is:%s\n",path);
+        char input[2048];
+        //read from user
+        fgets(input,2048,stdin);
+        //parse:
+        cmdLine* line = parseCmdLines(input);
+        
+    }
 }
